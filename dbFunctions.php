a29bb4e526fc86d1784af377051eedffe7395d08
19021227 <19021227@DESKTOP-D3PSHC3>
2021-02-23 20:58:40 +0800

    App 3 commit 2

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,11 +1,11 @@
 <?php
 
 $host = "localhost";
-$username = "root";
-$password = "";
-$db = "c203_p13";
-$link = mysqli_connect($host,$username,$password,$db) or 
-die(mysqli_connect_error());
+//$username = "root";
+//$password = "";
+//$db = "c203_p13";
+//$link = mysqli_connect($host,$username,$password,$db) or 
+//die(mysqli_connect_error());
 
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
