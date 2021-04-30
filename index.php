5e5fd9a04d36daa212d66c7465bccb78cf7dbb6e
19021227 <19021227@DESKTOP-D3PSHC3>
2021-04-30 18:01:34 +0800

    App 3 commit 7

--- index.php
+++ index.php
@@ -29,13 +29,13 @@ session_start();
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="images/maxresdefault.jpg" alt="First slide" style="height: 600px">
+                    <img class="d-block w-100" src="images/maxresdefault.jpg" alt="First slide" style="height: 700px">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="images/5c74358a1631a3614f2df5b4.jpg" alt="Second slide" style="height: 600px">
+                    <img class="d-block w-100" src="images/5c74358a1631a3614f2df5b4.jpg" alt="Second slide" style="height: 700px">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="images/5c0ad7c045972a242a429018.jpg" alt="Third slide" style="height: 600px">
+                    <img class="d-block w-100" src="images/5c0ad7c045972a242a429018.jpg" alt="Third slide" style="height: 700px">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
