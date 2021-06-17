81144fb3252908bf8b3fbbb85749422ad983415d
19021227 <19021227@DESKTOP-D3PSHC3>
2021-06-17 17:52:28 +0800

    App 3 commit 7

--- navbar.php
+++ navbar.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="js/navbar.js" type="text/javascript"></script>
 <nav class="navbar navbar-expand-md navbar-dark bg-danger">
     <a class="navbar-brand" href="index.php"><strong>Book Publisher <i class="fa fa-book-open" aria-hidden="true"></i></strong></a>
